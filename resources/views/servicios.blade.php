8eba8854a3e92453a562e6fe82338ff2da0ad782
BMACF12 <122118058+BMACF12@users.noreply.github.com>
2023-07-01 11:33:17 -0500

    BF
    cambios servicios

---
+++ resources/views/servicios.blade.php
@@ -0,0 +1,445 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Servicios</title>
+    <link href="{{ asset( 'css/app.css' ) }}" rel="stylesheet">
+    <!-- Enlace al archivo CSS de Tailwind -->
+    <link href="http://127.0.0.1:8000/css/app.css" rel="stylesheet">
+    <!--Enlace del archivo de los iconos-->
+    <script src="https://kit.fontawesome.com/2655b6ac6f.js" crossorigin="anonymous"></script>
+</head>
+<body>
+     <!-- Barra de navegación -->
+@component('layouts.components.navbar')@endcomponent
+<!--Formulario de contactos-->
+    <!-- Tailwind Play: https://play.tailwindcss.com/qIqvl7e7Ww  -->
+
+
+<!-- component -->
+<!-- ====== Cards Section Start -->
+<section class="flex justify-center pt-10 lg:pt-[50px] pb-10 lg:pb-10 bg-[#F3F4F6]">
+   <div class="container">
+      <div class="flex flex-wrap -mx-4">
+         <div class="w-full md:w-1/2 xl:w-1/3 px-4">
+            <div class="bg-white rounded-lg overflow-hidden mb-10">
+               <img
+                  src="https://cdn.tailgrids.com/1.0/assets/images/cards/card-01/image-01.jpg"
+                  alt="image"
+                  class="w-full"
+                  />
+               <div class="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
+                  <h3>
+                     <a
+                        href="javascript:void(0)"
+                        class="
+                        font-semibold
+                        text-dark text-xl
+                        sm:text-[22px]
+                        md:text-xl
+                        lg:text-[22px]
+                        xl:text-xl
+                        2xl:text-[22px]
+                        mb-4
+                        block
+                        hover:text-primary
+                        "
+                        >
+                     50+ Best creative website themes & templates
+                     </a>
+                  </h3>
+                  <p class="text-base text-body-color leading-relaxed mb-7">
+                     Lorem ipsum dolor sit amet pretium consectetur adipiscing
+                     elit. Lorem consectetur adipiscing elit.
+                  </p>
+                  <a
+                     href="javascript:void(0)"
+                     class="
+                     inline-block
+                     py-2
+                     px-7
+                     border border-[#E5E7EB]
+                     rounded-full
+                     text-base text-body-color
+                     font-medium
+                     hover:border-primary hover:bg-primary hover:text-white
+                     transition
+                     "
+                     >
+                  View Details
+                  </a>
+               </div>
+            </div>
+         </div>
+         <div class="w-full md:w-1/2 xl:w-1/3 px-4">
+            <div class="bg-white rounded-lg overflow-hidden mb-10">
+               <img
+                  src="https://cdn.tailgrids.com/1.0/assets/images/cards/card-01/image-02.jpg"
+                  alt="image"
+                  class="w-full"
+                  />
+               <div class="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
+                  <h3>
+                     <a
+                        href="javascript:void(0)"
+                        class="
+                        font-semibold
+                        text-dark text-xl
+                        sm:text-[22px]
+                        md:text-xl
+                        lg:text-[22px]
+                        xl:text-xl
+                        2xl:text-[22px]
+                        mb-4
+                        block
+                        hover:text-primary
+                        "
+                        >
+                     The ultimate UX and UI guide to card design
+                     </a>
+                  </h3>
+                  <p class="text-base text-body-color leading-relaxed mb-7">
+                     Lorem ipsum dolor sit amet pretium consectetur adipiscing
+                     elit. Lorem consectetur adipiscing elit.
+                  </p>
+                  <a
+                     href="javascript:void(0)"
+                     class="
+                     inline-block
+                     py-2
+                     px-7
+                     border border-[#E5E7EB]
+                     rounded-full
+                     text-base text-body-color
+                     font-medium
+                     hover:border-primary hover:bg-primary hover:text-white
+                     transition
+                     "
+                     >
+                  View Details
+                  </a>
+               </div>
+            </div>
+         </div>
+         <div class="w-full md:w-1/2 xl:w-1/3 px-4">
+            <div class="bg-white rounded-lg overflow-hidden mb-10">
+               <img
+                  src="https://cdn.tailgrids.com/1.0/assets/images/cards/card-01/image-03.jpg"
+                  alt="image"
+                  class="w-full"
+                  />
+               <div class="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
+                  <h3>
+                     <a
+                        href="javascript:void(0)"
+                        class="
+                        font-semibold
+                        text-dark text-xl
+                        sm:text-[22px]
+                        md:text-xl
+                        lg:text-[22px]
+                        xl:text-xl
+                        2xl:text-[22px]
+                        mb-4
+                        block
+                        hover:text-primary
+                        "
+                        >
+                     Creative Card Component designs graphic elements
+                     </a>
+                  </h3>
+                  <p class="text-base text-body-color leading-relaxed mb-7">
+                     Lorem ipsum dolor sit amet pretium consectetur adipiscing
+                     elit. Lorem consectetur adipiscing elit.
+                  </p>
+                  <a
+                     href="javascript:void(0)"
+                     class="
+                     inline-block
+                     py-2
+                     px-7
+                     border border-[#E5E7EB]
+                     rounded-full
+                     text-base text-body-color
+                     font-medium
+                     hover:border-primary hover:bg-primary hover:text-white
+                     transition
+                     "
+                     >
+                  View Details
+                  </a>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- ====== Cards Section End -->
+<!-- component -->
+<!-- ====== Cards Section Start -->
+<section class="flex justify-center pt-10 lg:pt-[20px] pb-10 lg:pb-10 bg-[#F3F4F6]">
+   <div class="container">
+      <div class="flex flex-wrap -mx-4">
+         <div class="w-full md:w-1/2 xl:w-1/3 px-4">
+            <div class="bg-white rounded-lg overflow-hidden mb-10">
+               <img
+                  src="https://cdn.tailgrids.com/1.0/assets/images/cards/card-01/image-01.jpg"
+                  alt="image"
+                  class="w-full"
+                  />
+               <div class="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
+                  <h3>
+                     <a
+                        href="javascript:void(0)"
+                        class="
+                        font-semibold
+                        text-dark text-xl
+                        sm:text-[22px]
+                        md:text-xl
+                        lg:text-[22px]
+                        xl:text-xl
+                        2xl:text-[22px]
+                        mb-4
+                        block
+                        hover:text-primary
+                        "
+                        >
+                     50+ Best creative website themes & templates
+                     </a>
+                  </h3>
+                  <p class="text-base text-body-color leading-relaxed mb-7">
+                     Lorem ipsum dolor sit amet pretium consectetur adipiscing
+                     elit. Lorem consectetur adipiscing elit.
+                  </p>
+                  <a
+                     href="javascript:void(0)"
+                     class="
+                     inline-block
+                     py-2
+                     px-7
+                     border border-[#E5E7EB]
+                     rounded-full
+                     text-base text-body-color
+                     font-medium
+                     hover:border-primary hover:bg-primary hover:text-white
+                     transition
+                     "
+                     >
+                  View Details
+                  </a>
+               </div>
+            </div>
+         </div>
+         <div class="w-full md:w-1/2 xl:w-1/3 px-4">
+            <div class="bg-white rounded-lg overflow-hidden mb-10">
+               <img
+                  src="https://cdn.tailgrids.com/1.0/assets/images/cards/card-01/image-02.jpg"
+                  alt="image"
+                  class="w-full"
+                  />
+               <div class="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
+                  <h3>
+                     <a
+                        href="javascript:void(0)"
+                        class="
+                        font-semibold
+                        text-dark text-xl
+                        sm:text-[22px]
+                        md:text-xl
+                        lg:text-[22px]
+                        xl:text-xl
+                        2xl:text-[22px]
+                        mb-4
+                        block
+                        hover:text-primary
+                        "
+                        >
+                     The ultimate UX and UI guide to card design
+                     </a>
+                  </h3>
+                  <p class="text-base text-body-color leading-relaxed mb-7">
+                     Lorem ipsum dolor sit amet pretium consectetur adipiscing
+                     elit. Lorem consectetur adipiscing elit.
+                  </p>
+                  <a
+                     href="javascript:void(0)"
+                     class="
+                     inline-block
+                     py-2
+                     px-7
+                     border border-[#E5E7EB]
+                     rounded-full
+                     text-base text-body-color
+                     font-medium
+                     hover:border-primary hover:bg-primary hover:text-white
+                     transition
+                     "
+                     >
+                  View Details
+                  </a>
+               </div>
+            </div>
+         </div>
+         <div class="w-full md:w-1/2 xl:w-1/3 px-4">
+            <div class="bg-white rounded-lg overflow-hidden mb-10">
+               <img
+                  src="https://cdn.tailgrids.com/1.0/assets/images/cards/card-01/image-03.jpg"
+                  alt="image"
+                  class="w-full"
+                  />
+               <div class="p-8 sm:p-9 md:p-7 xl:p-9 text-center">
+                  <h3>
+                     <a
+                        href="javascript:void(0)"
+                        class="
+                        font-semibold
+                        text-dark text-xl
+                        sm:text-[22px]
+                        md:text-xl
+                        lg:text-[22px]
+                        xl:text-xl
+                        2xl:text-[22px]
+                        mb-4
+                        block
+                        hover:text-primary
+                        "
+                        >
+                     Creative Card Component designs graphic elements
+                     </a>
+                  </h3>
+                  <p class="text-base text-body-color leading-relaxed mb-7">
+                     Lorem ipsum dolor sit amet pretium consectetur adipiscing
+                     elit. Lorem consectetur adipiscing elit.
+                  </p>
+                  <a
+                     href="javascript:void(0)"
+                     class="
+                     inline-block
+                     py-2
+                     px-7
+                     border border-[#E5E7EB]
+                     rounded-full
+                     text-base text-body-color
+                     font-medium
+                     hover:border-primary hover:bg-primary hover:text-white
+                     transition
+                     "
+                     >
+                  View Details
+                  </a>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+<!-- ====== Cards Section End -->
+<!-- component -->
+<body class="bg-gray-400">
+	<!-- Container -->
+	<div class="container mx-auto">
+		<div class="flex justify-center px-6 my-12">
+			<!-- Row -->
+			<div class="w-full xl:w-3/4 lg:w-11/12 flex">
+				<!-- Col -->
+				<div
+					class="w-full h-auto bg-gray-400 hidden lg:block lg:w-5/12 bg-cover rounded-l-lg"
+					style="background-image: url('https://source.unsplash.com/Mv9hjnEUHR4/600x800')"
+				></div>
+				<!-- Col -->
+				<div class="w-full lg:w-7/12 bg-white p-5 rounded-lg lg:rounded-l-none">
+					<h3 class="pt-4 text-2xl text-center">Create an Account!</h3>
+					<form class="px-8 pt-6 pb-8 mb-4 bg-white rounded">
+						<div class="mb-4 md:flex md:justify-between">
+							<div class="mb-4 md:mr-2 md:mb-0">
+								<label class="block mb-2 text-sm font-bold text-gray-700" for="firstName">
+									First Name
+								</label>
+								<input
+									class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+									id="firstName"
+									type="text"
+									placeholder="First Name"
+								/>
+							</div>
+							<div class="md:ml-2">
+								<label class="block mb-2 text-sm font-bold text-gray-700" for="lastName">
+									Last Name
+								</label>
+								<input
+									class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+									id="lastName"
+									type="text"
+									placeholder="Last Name"
+								/>
+							</div>
+						</div>
+						<div class="mb-4">
+							<label class="block mb-2 text-sm font-bold text-gray-700" for="email">
+								Email
+							</label>
+							<input
+								class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+								id="email"
+								type="email"
+								placeholder="Email"
+							/>
+						</div>
+						<div class="mb-4 md:flex md:justify-between">
+							<div class="mb-4 md:mr-2 md:mb-0">
+								<label class="block mb-2 text-sm font-bold text-gray-700" for="password">
+									Password
+								</label>
+								<input
+									class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border border-red-500 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+									id="password"
+									type="password"
+									placeholder="******************"
+								/>
+								<p class="text-xs italic text-red-500">Please choose a password.</p>
+							</div>
+							<div class="md:ml-2">
+								<label class="block mb-2 text-sm font-bold text-gray-700" for="c_password">
+									Confirm Password
+								</label>
+								<input
+									class="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+									id="c_password"
+									type="password"
+									placeholder="******************"
+								/>
+							</div>
+						</div>
+						<div class="mb-6 text-center">
+							<button
+								class="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+								type="button"
+							>
+								Register Account
+							</button>
+						</div>
+						<hr class="mb-6 border-t" />
+						<div class="text-center">
+							<a
+								class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
+								href="#"
+							>
+								Forgot Password?
+							</a>
+						</div>
+						<div class="text-center">
+							<a
+								class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800"
+								href="./index.html"
+							>
+								Already have an account? Login!
+							</a>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</html>
