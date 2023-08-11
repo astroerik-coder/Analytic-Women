585662c1fdc640adcfba36da347d2ab3021b35af
astroerik <dani008molina@gmail.com>
2023-08-10 22:12:04 -0500

    API
    Insert the routes and controller for use the API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\APIEventoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+}); 
+
+//CRUD de eventos
+Route::get('/eventos', [APIEventoController::class, 'obtenerEventos']);
+Route::get('/eventos/{id}', [APIEventoController::class, 'obtenerEventosId']);
+Route::post('/eventos', [APIEventoController::class, 'crearEvento']);
+Route::put('/eventos/{id}', [APIEventoController::class, 'actualizarEvento']);
+Route::delete('/eventos/{id}', [APIEventoController::class, 'eliminarEvento']);
+
+//Empleo
+Route::get('/empleos', [APIEventoController::class, 'obtenerEmpleos']);
+
+//Cursos
+Route::get('/cursos', [APIEventoController::class, 'obtenerCursos']);
+
+//Pago
+Route::get('/pagos', [APIEventoController::class, 'obtenerPagos']);
+
+//Empresas
+Route::get('/empresas', [APIEventoController::class, 'obtenerEmpresas']);
